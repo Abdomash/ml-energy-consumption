644f17486949964e9eb07fe10083c5b85727b91e
Abdallah Al-Sukhni <abdullahalsukhni@gmail.com>
2024-04-17 17:48:51 -0500

    Data Pushed

--- run_tests.py
+++ run_tests.py
@@ -21,7 +21,7 @@ dataset = {
     "CDCHealthIndicator": (fetch_ucirepo(id=891).data.features, fetch_ucirepo(id=891).data.targets)
 }
 # Set the folder name to where the results will be saved as an environment variable
-os.environ['DEVICE_NAME'] = 'abdo-pc' # Change this to the name of your device
+os.environ['DEVICE_NAME'] = 'Abdallah-pc' # Change this to the name of your device
 
 if not os.path.exists(os.path.join('results',os.environ['DEVICE_NAME'])):
     os.makedirs(os.path.join('results',os.environ['DEVICE_NAME']))
